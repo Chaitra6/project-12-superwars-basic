3fd5791c50a35a466ca0b94c05ec6f4f9b93f857
Chaitra6 <48185350+Chaitra6@users.noreply.github.com>
2020-08-12 16:17:51 +0530

    Prograd S-61

--- src/app.js
+++ src/app.js
@@ -1,92 +1,77 @@
 const PLAYERS = [
-    "Spiderman",
-    "Captain America",
-    "Wonderwoman",
-    "Popcorn",
-    "Gemwoman",
-    "Bolt",
-    "Antwoman",
-    "Mask",
-    "Tiger",
-    "Captain",
-    "Catwoman",
-    "Fish",
-    "Hulk",
-    "Ninja",
-    "Black Cat",
-    "Volverine",
-    "Thor",
-    "Slayer",
-    "Vader",
-    "Slingo"
+  "Spiderman",
+  "Captain America",
+  "Wonderwoman",
+  "Popcorn",
+  "Gemwoman",
+  "Bolt",
+  "Antwoman",
+  "Mask",
+  "Tiger",
+  "Captain",
+  "Catwoman",
+  "Fish",
+  "Hulk",
+  "Ninja",
+  "Black Cat",
+  "Volverine",
+  "Thor",
+  "Slayer",
+  "Vader",
+  "Slingo",
 ];
-
+let detailedPlayers = [];
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = [];
-    // Create players using for loop
-    // Type your code here
-    for (var i = 0; i < PLAYERS.length; i++) {
-        detailedPlayers[i] = PLAYERS[i]
-        if (i % 2 == 0) {
-            detailedPlayers[i] = {
+  for (var i = 0; i < PLAYERS.length; i++) {
+    var players = {
+      name: PLAYERS[i],
+      strenght: getRandomStrength(),
+      image: "./images/super-"(i + 1) + ".png",
+      type: "hero|villian",
+    };
+    detailedPlayers.push(players);
+  }
 
-                {
-                    images: /images/super - 1
-                },
-                {
-                    strength: getRandomStrength()
-                },
-                {
-                    type: Hero
-                }
-
-            };
-        } else {
-            detailedPlayers[i] = {
-
-                {
-                    images: /images/super - 1
-                },
-                {
-                    strength: getRandomStrength()
-                },
-                {
-                    type: Villian
-                }
-
-            };
-        }
-
-    }
-
-    return detailedPlayers;
-}
+  return detailedPlayers;
+};
 
 // getting random strength
 const getRandomStrength = () => {
-    // Return a random integer (0,100]
-    // Note: You can use Math.random() and Math.ceil()
-    return Math.floor(Math.random() * 101);
-}
+  // Return a random integer (0,100]
+  // Note: You can use Math.random() and Math.ceil()
+  return Math.floor(Math.random() * 101);
+};
 
 const buildPlayers = (players, type) => {
-    let fragment = '';
-
-    // Loop through players and accumulate HTML template
-    // depending of type of player(hero|villain)
-    // Type your code here
-
-    return fragment;
-}
+  let fragment = "";
+  for (var i = 0; i < PLAYERS.length; i++) {
+    fragment = `
+        <div class="player>
+            <img src ="${detailedPlayers[i].image}" alt="character">
+            <h3> ${detailedPlayers[i].name} </h3>
+            <h4> ${detailedPlayers[i].strenght}</h4>
+        </div> `
+  }
+  return fragment;
+};
 // Display players in HTML
 const viewPlayers = (players) => {
+  let display = "";
+  document.getElementById("heroes").innerHTML = buildPlayers(players, "hero");
+  document.getElementById("villains").innerHTML = buildPlayers(
+    players,
+    "villain"
+  );
+  display = ` <div>
+  <p> ${detailedPlayers} </p>
+  </div> `
+  return display;
 
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
 
-}
+
+};
 
 window.onload = () => {
-    viewPlayers(initPlayers(PLAYERS));
-}
+  viewPlayers(initPlayers(PLAYERS));
+};
